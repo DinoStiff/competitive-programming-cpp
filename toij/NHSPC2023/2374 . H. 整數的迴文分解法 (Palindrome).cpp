0d81372299de6ed1f3835414b1e9136a9e7c527e
DinoStiff <112380363+DinoStiff@users.noreply.github.com>
2026-09-22 15:04:19 +0800

    Add toij/NHSPC2023 competitive-programming solutions

---
+++ toij/NHSPC2023/2374 . H. 整數的迴文分解法 (Palindrome).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int mod_pow(int a, long long int n) 
+{
+    if (n == 0) return 1;
+    int half = mod_pow(a, n / 2);
+    half = (long long int)half * half % mod;
+    if (n % 2 == 1) half = (long long int)half * a % mod;
+    return half;
+}
+
+void solve()
+{
+    long long int n;
+    cin >> n;
+    cout << mod_pow(2, n / 2) << "\n";
+}
+
+int main() 
+{
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
